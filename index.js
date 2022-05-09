ff2f6c1769997a27c69990c84cdbfad1b95ea27a
Shane Tierney <sptierney713@gmail.com>
2022-05-08 22:31:44 -0400

    styles everything

--- index.js
+++ index.js
@@ -4,22 +4,22 @@ const showdata = require('./showdata')
 const app = express()
 
 app.set('view engine', 'pug')
+app.use(express.static('public'))
 
 app.get('/', (request, response) => {
-    return response.render('index', { showdata })
+  return response.render('index', { showdata })
 })
 
 app.get('/seasons/:number', (request, response) => {
-    const season = showdata.seasons.find((season) => { return season.number === parseInt(request.params.number)})
-    return response.render('seasons', { season, 'title': showdata.title })
-    
-    //return response.send("test")
+  const season = showdata.seasons.find((season) => { return season.number === parseInt(request.params.number) })
+
+  return response.render('seasons', { season, 'title': showdata.title })
 })
 
 app.all('*', (require, response) => {
-    return response.sendStatus(404)
+  return response.sendStatus(404)
 })
 
 app.listen(1337, () => {
-    console.log('Application is up: http://localhost:1337')
+  console.log('Application is up: http://localhost:1337')
 })
